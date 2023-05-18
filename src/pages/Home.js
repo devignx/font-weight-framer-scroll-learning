8a4a9f30c85d666964f5d1a08edfe8ffa6b295ad
Devign Experience <devignx@gmail.com>
2023-05-18 17:39:18 +0530

    carousel trial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,51 @@
-import ThemeSwitch from '../components/ThemeSwitch'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Carousel.css'; // Import CSS file for styling
 
-const Home = () => {
+const Home = ({ images = Array.from({ length: 6 }, (_, index) => `https://picsum.photos/300/200?text=Image${index + 1}`) }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [slide, setSlide] = useState(true);
+
+  useEffect(() => {
+    if (images && images.length > 0 && slide === true) {
+      const interval = setInterval(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, 3000);
+
+      return () => clearInterval(interval);
+    }
+  }, [images,slide]);
+
+  const renderIndicators = () => {
+    if (images && images.length > 0) {
+      return images.map((_, index) => (
+        <span
+          key={index}
+          className={`indicator ${index === currentImageIndex ? 'active' : ''}`}
+          onClick={() => setCurrentImageIndex(index)}
+        />
+      ));
+    }
+    return null;
+  };
+
   return (
-    <div className="h-screen w-full flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold text-center mt-8">Welcome to Theme & <br/>Accent Switcher</h1>
-      <p className="text-lg mt-4">Choose any one of the colours</p>
-      <ThemeSwitch />
+    <div className="relative pb-16 bg-black p-6">
+      <div className="image-container p-2 bg-red-500 overflow-x-scroll">
+        {
+          images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`${index}`}
+              className={`carousel-image ${index === currentImageIndex ? 'active' : ''}`}
+            />
+          ))
+        }
+      </div>
+      <div onClick={()=>{setSlide(false)}} className="indicators ">{renderIndicators()}</div>
     </div>
   );
-}
+};
+
 
 export default Home;
